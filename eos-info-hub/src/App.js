2cd5c399b1d68a6f354a7bc726f15b53ed8a7be9
tcpaul <tcpaul9@gmail.com>
2018-05-04 14:27:48 +0000

    got it all working and connected to 45.33.125.144:8888

--- eos-info-hub/src/App.js
+++ eos-info-hub/src/App.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import * as Eos from 'eosjs';
 
-const eos = Eos.Testnet();
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
 
-eos.getBlock();
+const config = {
+httpEndpoint: 'http://45.33.125.144:8888'
+}
+
+const eos = Eos.Testnet(config);
+
+eos.getBlock(1).then(result => {console.log(result)});
+
+eos.getBlock({block_num_or_id: 1}).then(result => console.log(result))
+
+// Callbacks are similar
+const callback = (err, res) => {err ? console.error(err) : console.log(res)}
+eos.getBlock(1, callback)
+eos.getBlock({block_num_or_id: 1}, callback)
+
+// Provide an empty object or a callback if an API call has no arguments
+eos.getInfo({}).then(result => {console.log(result)})
 
 class App extends Component {
+
+getInfo = () => {
+eos.getInfo({}).then(result => {console.log(result)})
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+       <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+<Button variant="raised" color="primary" onClick={this.getInfo}>
+      GET INFO
+    </Button>
+<Paper>
+Make all the eos
+</Paper>
       </div>
     );
   }
